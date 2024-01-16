84a897f2e78a0a4a76433ab9376cb91784324c01
JaeDragon <leejy0068@gmail.com>
2024-01-16 09:30:34 +0900

    Create ViewController.m

---
+++ Hybrid/InAppBrowser/ViewController.m
@@ -0,0 +1,19 @@
+// Handle the window.open request
+- (WKWebView *)webView:(WKWebView *)webView createWebViewWithConfiguration:(WKWebViewConfiguration *)configuration forNavigationAction:(WKNavigationAction *)navigationAction windowFeatures:(WKWindowFeatures *)windowFeatures {
+    
+    //[[UIApplication sharedApplication] openURL:navigationAction.request.URL options:@{} completionHandler:nil];
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    InAppBrowserViewController *inAppBrowserViewController = [storyboard instantiateViewControllerWithIdentifier:@"InAppBrowserView"];
+
+    // 프로퍼티에 데이터 설정
+    inAppBrowserViewController.urlString = navigationAction.request.URL;
+    
+    // 모달 프레젠테이션 스타일을 풀스크린으로 설정
+    inAppBrowserViewController.modalPresentationStyle = UIModalPresentationFullScreen;
+    [self presentViewController:inAppBrowserViewController animated:NO completion:^{
+        
+    }];
+    
+    return false;
+}
