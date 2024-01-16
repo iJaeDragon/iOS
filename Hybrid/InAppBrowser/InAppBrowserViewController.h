ec7216f5c5652a4b0a50752b048ba24bb476ed20
JaeDragon <leejy0068@gmail.com>
2024-01-16 09:29:19 +0900

    Update InAppBrowserViewController.h

--- Hybrid/InAppBrowser/InAppBrowserViewController.h
+++ Hybrid/InAppBrowser/InAppBrowserViewController.h
@@ -1,1 +1,17 @@
+#ifndef InAppBrowser_h
+#define InAppBrowser_h
 
+#import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
+
+#endif /* InAppBrowser_h */
+
+@interface InAppBrowserViewController : UIViewController <WKUIDelegate>
+
+@property (nonatomic, strong) NSString *urlString;
+
+@property (weak, nonatomic) IBOutlet WKWebView *webView;
+
+- (IBAction) inAppBrowserViewClose:(id)sender;
+
+@end
